7d1a84ae3e849123bf983f243495215f60940387
hoseacodes <dominique11h@yahoo.com>
2021-04-24 14:59:22 -0500

    About Page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,48 +3,64 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className="container-fluid">
-            <div className="row footer-top">
-                <div className="col-sm-3 ">
-                    <ul className="list-unstyled mt-3">
-                        <li>Keep in Touch</li>
-                        <div className="mb-2"></div>
-                        <li>Social Media</li>
-                        <li>Made By <a href="">HoseaCodes</a> </li>
-                    </ul>
-                </div>
-                <div className="col-sm-3 border-left">
-                    <ul className="list-unstyled mt-3">
-                        <li><a href="#" >Cookie Policy</a></li>
-                        <div className="mb-2"></div>
-                        <li><a href="#" >Privacy Policy</a></li>
-                        <div className="mb-2"></div>
-                        <li><a href="#" >Terms & Conditions</a></li>
-                    </ul>
-                    <ul className="list-unstyled mt-3">
-                        <li>cpoyright 2021 Poltically Savvy LLC</li>
-                        <div className="mb-2"></div>
-                        <li>All  Rights Reserved</li>
-                    </ul>
-                </div>
-                <div className="col-sm-3 col-xs-12  border-left">
-                    <ul className="list-unstyled mt-3">
-                        <li><a href="#" >About</a></li>
-                        <div className="mb-2"></div>
-                        <li><a href="#" >Contact</a></li>
-                    </ul>
-                </div>
+        <div className="footer-top">
+            <div className="container" >
+                <div className="row">
+                    <div className="col-sm-3 ">
+                        <ul className="list-unstyled mt-3">
+                            <li>Subscribe to our newsletter</li>
+                            <div className="mb-2"></div>
+                            <input className="footer-input"
+                                placeholder="Email Address"
+                                type="email" name="" />
+                            <br />
+                            <a href="">Sign Up</a>
+                            <p>By clicking SIGN UP, you agree to receive emails from Poosh and agree to our terms of use and privacy policy.</p>
+                            <li>Made By <a href="">HoseaCodes</a> </li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-3 border-left">
+                        <ul className="list-unstyled mt-3">
+                            <li><a href="#" >Cookie Policy</a></li>
+                            <div className="mb-2"></div>
+                            <li><a href="#" >Privacy Policy</a></li>
+                            <div className="mb-2"></div>
+                            <li><a href="#" >Terms & Conditions</a></li>
+                        </ul>
+                        <ul className="list-unstyled mt-3">
+                            <li>&copy; Poltically Savvy LLC</li>
+                            <div className="mb-2"></div>
+                            <li>All  Rights Reserved</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-3 col-xs-12  border-left">
+                        <ul className="list-unstyled mt-3">
+                            <li><a href="#" >About</a></li>
+                            <div className="mb-2"></div>
+                            <li><a href="#" >Contact</a></li>
+                        </ul>
+                    </div>
 
-                <div className="col-sm-3 border-left">
-                    <ul className="list-unstyled mt-3">
-                        <li>Keep in Touch</li>
-                        <div className="mb-2"></div>
-                        <li>Social Media</li>
-                        <li>Made By <a href="">HoseaCodes</a> </li>
-                    </ul>
+                    <div className="col-sm-3 border-left">
+                        <ul className="list-unstyled mt-3">
+                            <li>Keep in Touch</li>
+                            <div className="mb-2"></div>
+                            <div class="d-flex flex-column pl-md-2 pb-3">
+                                <div class="row d-flex pl-2">
+                                    <li class="footer-social">
+                                        <span> <i class="fa fa-facebook" aria-hidden="true"></i></span>
+                                        <span> <i class="fa fa-instagram" aria-hidden="true"></i> </span>
+                                        <span> <i class="fa fa-twitter" aria-hidden="true"></i> </span>
+                                    </li>
+                                </div>
+                            </div>
+                            <li>Made By <a href="">HoseaCodes</a> </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
+
     )
 }
 
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './About.css';
+import about from '../../Images/about.png'
+import aboutQuote from '../../Images/about-quote.png'
 import NavBar from '../../Components/NavBar/NavBar';
 import Footer from '../../Components/Footer/Footer';
 
@@ -7,6 +9,36 @@ const About = () => {
     return (
         <>
             <NavBar />
+            <div className="container">
+                <div className="about-container">
+
+                    <img className="about-img" src={about} alt="Founder" />
+                    <div className="about-text">
+                        <h2>Welcome to Politically Savvy</h2>
+                        <p>For years, I have served as a useful source to those seeking creative inspiration, innovate help, and personal growth advice. I finally decided to own that role and be intentional about it.
+                        I started writing about my experiences, thoughts, and curious wonders of the world; while creatively incorporating my educational background.  With a BA in political science and journalism, I've become a expert in behavioral science.
+                        I founded PoliticallySavvy.org with a mission to give others a taste of what goes on in my mind, and offer my services to those seeking professional and personal assistance.
+                        Take some time to explore the blog, read something interesting, and feel free to reach out if you would like to collaborate on a project together.
+</p>
+                        <p className="about-signoff">Peace & Love, <br /> <span> Altremese Banks</span> </p>
+                    </div>
+                </div>
+            </div>
+            <hr className="about-hr" />
+            <div className="about-quote-container">
+                <div className="about-quote-div">
+                    <h2 className="about-believe-title">What We Believe</h2>
+                    <p className="about-quote">
+                        <em>
+                            "The most exemting, challenging, and significant relationship of all is the one you have with yourself. And if you find someone to love the you you love, well that's just fabulous."
+                        </em>
+                    </p>
+                    <p className="about-quote name">- Carrie Bradshaw </p>
+
+                </div>
+                <img src={aboutQuote} alt="Founder" />
+            </div>
+
             <Footer />
         </>
     )
