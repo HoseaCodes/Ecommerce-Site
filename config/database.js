a5b8eb5ae70b07dc6c7a09eb57b9a2f64c0b18d3
jasperisaac27 <jasperisaac27@gmail.com>
2021-05-14 13:28:50 +0800

    Database info removed

--- config/database.js
+++ config/database.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 // Connect to Mongodb
 const URI =
 	process.env.MONGODB_URL ||
-	`mongodb+srv://froschk12:Contristati2@cluster0.ia0tn.mongodb.net/politically-savvy?retryWrites=true&w=majority`;
-mongoose.connect(URI, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-});
+	mongoose.connect(URI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	});
 const db = mongoose.connection;
 // database connection event
 db.on("connected", function () {
